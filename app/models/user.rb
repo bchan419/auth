87af7ac6d00968e40af6be169459aa10027fa77d
Student <student@codeacademy.org>
2012-10-26 17:16:28 -0500

    fix minor bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   
   validates_presence_of :email  
-  validates_uniqueness_of :def email(args)
-    
-  end
-  
+  validates_uniqueness_of :email    
   
 end
